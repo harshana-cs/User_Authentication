8efacd52ffe8b6fa08d8a6d62aac9e44491fc672
Harshana Bhandari <np03cs4a230186@heraldcollege.edu.np>
2025-06-23 13:54:11 +0545

    random things are installed

--- controllers/authcontrollers.js
+++ controllers/authcontrollers.js
@@ -1,5 +1,7 @@
 const bcrypt = require('bcryptjs');
 const { getUsers, saveUsers } = require('../utilis/userStorage');
+const jwt = require('jsonwebtoken');
+const SECRET = 'your_jwt_secret';
 
 function signup(req, res) {
   const { username, password } = req.body;
@@ -15,5 +17,22 @@ function signup(req, res) {
   saveUsers(users);
   res.status(201).json({ msg: 'User registered successfully' });
 }
+function login(req, res) {
+  const { username, password } = req.body;
+  const users = getUsers();
 
-module.exports = { signup };
+  const user = users.find(u => u.username === username);
+  if (!user) {
+    return res.status(404).json({ msg: 'User not found' });
+  }
+
+  const isMatch = bcrypt.compareSync(password, user.password);
+  if (!isMatch) {
+    return res.status(401).json({ msg: 'Invalid password' });
+  }
+
+  const token = jwt.sign({ username: user.username }, SECRET, { expiresIn: '1h' });
+
+  res.json({ msg: 'Login successful', token });
+}
+module.exports = { signup, login };
